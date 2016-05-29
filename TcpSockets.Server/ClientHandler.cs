a48508d12aef8cb59dffa0071002b4257af13a15
ANTON PRUDKOHLIAD <antonprudkoglyad@gmail.com>
2016-05-29 23:06:24 +0300

    client handler class

---
+++ TcpSockets.Server/ClientHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using TcpSockets.Common;
+
+namespace TcpSockets.Server
+{
+    public class ClientHandler
+    {
+        private const string timeCommand = "time";
+
+        private readonly Socket _client;
+
+        public ClientHandler(Socket client)
+        {
+            _client = client;
+        }
+
+        public void Handle()
+        {
+            Logger.Log($"Connection accepted. Client ip: {_client.RemoteEndPoint}");
+
+            var request = ReadRequest(_client);
+
+            WriteResponse(_client, "[Welcome to my server :3] ");
+
+            if (request.Trim().ToLower() == timeCommand)
+            {
+                WriteResponse(_client, $"System date: {DateTime.Now.ToString()}");
+            }
+            else
+            {
+                WriteResponse(_client, request.ToUpper());
+            }
+        }
+
+        private static string ReadRequest(Socket client)
+        {
+            string data;
+            byte[] buffer = new byte[1500];
+
+            int size = client.Receive(buffer);
+            data = Encoding.UTF8.GetString(buffer, 0, size);
+
+            return data;
+        }
+
+        private static void WriteResponse(Socket client, string response)
+        {
+            var encodedMessage = Encoding.UTF8.GetBytes(response);
+            try
+            {
+                client.Send(encodedMessage);
+            }
+            catch (SocketException ex)
+            {
+                Logger.Log(ex.Message);
+            }
+        }
+    }
+}
--- TcpSockets.Server/Program.cs
+++ TcpSockets.Server/Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using TcpSockets.Common;
 
-namespace WebSocketLab
+namespace TcpSockets.Server
 {
     public class Program
     {
         private const string serverIpAddress = "127.0.0.1";
         private static int serverPort = 3000;
-        private const string timeCommand = "time";
-        private const string response = "Welcome to my server <3";
 
         public static void Main(string[] args)
         {
@@ -30,53 +27,11 @@ namespace WebSocketLab
 
                 Task.Run(() =>
                 {
-                    HandleClient(client);
+                    new ClientHandler(client).Handle();
                 });
             }
 
             Console.Read();
-        }
-
-        private static void HandleClient(Socket client)
-        {
-            Logger.Log($"Connection accepted. Client ip: {client.RemoteEndPoint}");
-
-            var request = ReadRequest(client);
-
-            WriteResponse(client, "[Welcome to my server :3] ");
-
-            if (request.Trim().ToLower() == timeCommand)
-            {
-                WriteResponse(client, $"System date: {DateTime.Now.ToString()}");
-            }
-            else
-            {
-                WriteResponse(client, request.ToUpper());
-            }
-        }
-
-        private static string ReadRequest(Socket client)
-        {
-            string data;
-            byte[] buffer = new byte[1500];
-
-            int size = client.Receive(buffer);
-            data = Encoding.UTF8.GetString(buffer, 0, size);
-
-            return data;
-        }
-
-        private static void WriteResponse(Socket client, string response)
-        {
-            var encodedMessage = Encoding.UTF8.GetBytes(response);
-            try
-            {
-                client.Send(encodedMessage);
-            }
-            catch (SocketException ex)
-            {
-                Logger.Log(ex.Message);
-            }
         }
     }
 }
