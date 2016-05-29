13101e6a2b0f5ed021e4ec5e8665351303d10209
ANTON PRUDKOHLIAD <antonprudkoglyad@gmail.com>
2016-05-29 22:57:57 +0300

    init projects

---
+++ TcpSockets.Common/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TcpSockets.Common
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            var prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write($"[{DateTime.UtcNow}] ");
+            Console.ForegroundColor = prevColor;
+            Console.WriteLine($"{message}");
+        }
+    }
+}
